ae472ed9e0bcf189f20ea4ec70e9b0dd4d8f217f
Hui Min Ang <whatrubbishisthis@gmail.com>
2020-02-17 20:42:55 +0000

    Updated feature_engineering.py

--- feature_engineering.py
+++ feature_engineering.py
@@ -18,5 +18,24 @@ test_df.drop(columns="Id", inplace=True)
 
 # deleting outliers
 train_df.drop(train_df[(train_df.GrLivArea > 4000)
-                       and (train_df.SalePrice < 300000)].index, inplace=True)
+                       & (train_df.SalePrice < 300000)].index, inplace=True)
+
+# transform SalePrice using log
+train_df["SalePrice"] = np.log(train_df["SalePrice"])
+
+# visualise the transformed SalePrice distribution
+sns.distplot(train_df["SalePrice"], fit=norm)
+plt.ylabel("Frequency")
+plt.title("SalePrice distribution")
+
+fig = plt.figure()
+res = stats.probplot(train_df.SalePrice, plot=plt)
+plt.show()
+
+# save SalePrice as train_y and drop it as we want to have metrics columns only
+train_y = train_df["SalePrice"]
+train_df.drop(columns="SalePrice", inplace=True)
+
+# dealing with missing values
+full_df = pd.concat([train_df, test_df], ignore_index=True)
 
