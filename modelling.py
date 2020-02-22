559e8a12cda17b5ddbb2e4a176f07c509186159d
Hui Min Ang <whatrubbishisthis@gmail.com>
2020-02-22 12:01:33 +0000

    Updated modelling.py

--- modelling.py
+++ modelling.py
@@ -1,12 +1,11 @@
 import pandas as pd
 import numpy as np
-from sklearn.linear_model import ElasticNet, Lasso, BayesianRidge
-from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, StackingRegressor
+from sklearn.linear_model import ElasticNet, Lasso
+from sklearn.ensemble import GradientBoostingRegressor, StackingRegressor
 from sklearn.kernel_ridge import KernelRidge
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import RobustScaler
-from sklearn.base import BaseEstimator, TransformerMixin, RegressorMixin, clone
-from sklearn.model_selection import KFold, cross_val_score, train_test_split
+from sklearn.model_selection import KFold, cross_val_score
 import xgboost as xgb
 import lightgbm as lgb
 
